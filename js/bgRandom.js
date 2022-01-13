0eb4e031be4cfed424f143e1c15f40a924e43d7a
Gregory Wiedeman <gregory.wiedeman1@gmail.com>
2022-01-13 16:37:25 -0500

    porting over more pages

--- js/bgRandom.js
+++ js/bgRandom.js
@@ -24,34 +24,24 @@ $(function(){
 
 $(function(){
 	if ($("body").css("background-image") != 'none')  {
-		if ($('#espyBG').length > 0) {
-			if ($(window).width() <= 1200) {
-				$("body").css('background-image', "url('https://archives.albany.edu/web/oldDrupal/img/1000/ndpa1.jpg')");
-			} else if ($(window).width() <= 1500) {
-				$("body").css('background-image', "url('https://archives.albany.edu/web/oldDrupal/img/1200/ndpa1.jpg')");
-			} else if ($(window).width() <= 1800) {
-				$("body").css('background-image', "url('https://archives.albany.edu/web/oldDrupal/img/1600/ndpa1.jpg')");
-			} else {
-				$("body").css('background-image', "url('https://archives.albany.edu/web/oldDrupal/img/1900/ndpa1.jpg')");
-			}
+		var $randomBG = Math.floor(Math.random() * ((43-1)+1) + 1);
+		if ($(window).width() <= 1200) {
+			$("body").css('background-image', "url('https://archives.albany.edu/browse/img/1000/bg" + $randomBG + ".jpg')");
+		} else if ($(window).width() <= 1500) {
+			$("body").css('background-image', "url('https://archives.albany.edu/browse/img/1200/bg" + $randomBG + ".jpg')");
+		} else if ($(window).width() <= 1800) {
+			$("body").css('background-image', "url('https://archives.albany.edu/browse/img/1500/bg" + $randomBG + ".jpg')");
 		} else {
-			var $randomBG = Math.floor(Math.random() * ((43-1)+1) + 1);
-			if ($(window).width() <= 1200) {
-				$("body").css('background-image', "url('https://archives.albany.edu/browse/img/1000/bg" + $randomBG + ".jpg')");
-			} else if ($(window).width() <= 1500) {
-				$("body").css('background-image', "url('https://archives.albany.edu/browse/img/1200/bg" + $randomBG + ".jpg')");
-			} else if ($(window).width() <= 1800) {
-				$("body").css('background-image', "url('https://archives.albany.edu/browse/img/1500/bg" + $randomBG + ".jpg')");
-			} else {
-				$("body").css('background-image', "url('https://archives.albany.edu/browse/img/1800/bg" + $randomBG + ".jpg')");
-			}
+			$("body").css('background-image', "url('https://archives.albany.edu/browse/img/1800/bg" + $randomBG + ".jpg')");
 		}
 	}
 });
 
 $(function(){
-	var $randomImage = Math.floor(Math.random() * ((13-1)+1) + 1);
-	//$imgURL = window.location.protocol + "//" + window.location.hostname + "/web/img/main/fill" + $randomImage + ".jpg";
-	$imgURL = $("section.fill").css('background-image').split("fill")[0] + "fill" + $randomImage + ".jpg"
-	$("section.fill").css('background-image', $imgURL);
+	if ($('section.fill').length) {
+		var $randomImage = Math.floor(Math.random() * ((13-1)+1) + 1);
+		//$imgURL = window.location.protocol + "//" + window.location.hostname + "/web/img/main/fill" + $randomImage + ".jpg";
+		$imgURL = $("section.fill").css('background-image').split("fill")[0] + "fill" + $randomImage + ".jpg"
+		$("section.fill").css('background-image', $imgURL);
+	}
 });
