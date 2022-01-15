d6f598fed84aaa4633212518903e4218653d9e16
gwiedeman <gregory.wiedeman1@gmail.com>
2022-01-14 21:34:24 -0500

    setup error pages

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+jekyll build
+
+cp _site/index.html /var/www/hyrax-UAlbany/public
+cp _site/404.html /var/www/hyrax-UAlbany/public
+cp _site/422.html /var/www/hyrax-UAlbany/public
+cp _site/500.html /var/www/hyrax-UAlbany/public
+
+cp _site/index.html /var/www/bento-UAlbany/public
+cp _site/404.html /var/www/bento-UAlbany/public
+cp _site/422.html /var/www/bento-UAlbany/public
+cp _site/500.html /var/www/bento-UAlbany/public
+
+cp _site/index.html /var/www/history/public
+cp _site/404.html /var/www/history/public
+cp _site/422.html /var/www/history/public
+cp _site/500.html /var/www/history/public
+
+cp _site/index.html /var/www/books/public
+cp _site/404.html /var/www/books/public
+cp _site/422.html /var/www/books/public
+cp _site/500.html /var/www/books/public
+
+cp _site/index.html /var/www/update/arclight-UAlbany/public
+cp _site/404.html /var/www/update/arclight-UAlbany/public
+cp _site/422.html /var/www/update/arclight-UAlbany/public
+cp _site/500.html /var/www/update/arclight-UAlbany/public
