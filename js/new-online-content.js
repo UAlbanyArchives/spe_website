91d2db5c3895b4b818b610ffee331fdb76f4d45c
Gregory Wiedeman <gregory.wiedeman1@gmail.com>
2024-01-19 12:19:21 -0500

    basic card implementation without series titles

--- js/new-online-content.js
+++ js/new-online-content.js
@@ -1,7 +1,7 @@
 (function() {
 
 // Make a GET request to the JSON API
-fetch('https://archives.albany.edu/catalog?locale=en&per_page=500&format=json&search_field=all_fields')
+fetch('https://archives.albany.edu/catalog?page=3&per_page=500&format=json&search_field=all_fields')
   .then(response => {
     // Check if the request was successful (status code 200)
     if (!response.ok) {
@@ -71,25 +71,24 @@ function createCardHTML(result) {
   const collectingAreaAbbreviation = collectingAreaLookup[result.collecting_area_tesim] || result.collecting_area_tesim;
 
   return `
-    <div class="newContent">
-        <div class="contentTitle">
-            <h4>
-                <a href="https://archives.albany.edu/concern/${resourceModel}s/${result.id}">${result.title_tesim}</a><br />
-                <small><a href="https://archives.albany.edu/description/catalog/${result.collection_number_tesim}">${result.collection_tesim}</a></small>
-            </h4>
-            <div class="lowerContent row">
-                <div class="metadata col-xs-12 col-sm-10">
-                    <p class=""><strong>Series:</strong> <a href="">${result.resource_type_tesim}</a></p>
-                    <p class=""><strong>Date:</strong> ${result.date_created_tesim}</p>
-                    <p class=""><strong>Type:</strong> ${result.resource_type_tesim}</p>
-                    <p class=""><a href="https://archives.albany.edu/description/repositories/${collectingAreaAbbreviation}">${result.collecting_area_tesim}</a></p>
-                </div>
-                <div class="galleryThumb entry col-xs-12 col-sm-2">
-                    <img class="galleryImage" src="https://archives.albany.edu/${result.thumbnail_path_ss}" alt="thumbnail for ${result.title_tesim}">
-                </div>
+    <div class="card mb-3" style="max-width: 540px;">
+        <div class="row no-gutters">
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title"><a href="https://archives.albany.edu/concern/${resourceModel}s/${result.id}">${result.title_tesim}</a></h5>
+              <p class="card-text"><a href="https://archives.albany.edu/description/catalog/${result.collection_number_tesim}">${result.collection_tesim}</a></p>
+              <p class="card-text">
+                <strong>Date:</strong> ${result.date_created_tesim}<br />
+                <strong>Type:</strong> ${result.resource_type_tesim}<br />
+                <strong>Series:</strong> <a href="https://archives.albany.edu/description/catalog/${result.collection_number_tesim}aspace_${result.record_parent_tesim}">${result.record_parent_tesim}</a><br />
+              </p>
+              <p class="card-text"><span class="text-muted"><a href="https://archives.albany.edu/description/repositories/${collectingAreaAbbreviation}">${result.collecting_area_tesim}</a></span></p>
             </div>
+          </div>
+          <div class="col-md-4 d-flex align-items-center">
+            <img class="card-img" src="https://archives.albany.edu/${result.thumbnail_path_ss}" alt="thumbnail for ${result.title_tesim}">
+          </div>
         </div>
-        
     </div>
   `;
 }
